41b5b7d71b8e3d055a0d048f46a137bc95781233
Lotnest <lotnestyt@gmail.com>
2021-03-29 21:07:03 +0200

    Added starter challenges

---
+++ src/lotnest/challenges/hackerrank/java/StdInAndStdOutI.java
@@ -0,0 +1,16 @@
+package lotnest.challenges.hackerrank.java;
+
+import java.util.Scanner;
+
+public class StdInAndStdOutI {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        System.out.println(a);
+        a = scanner.nextInt();
+        System.out.println(a);
+        a = scanner.nextInt();
+        System.out.println(a);
+    }
+}
